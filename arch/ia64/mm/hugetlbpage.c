11231d5d5170c5c073cc6b3fa899b43bb88cfa44
Johnny Mnemonic <jm@machine-hall.org>
2024-11-24 11:06:09 +0100

    ia64: replay "mm: drop hugetlb_get_unmapped_area{_*} functions"
    See cc92882ee218d62ef017fa545b3c8a2d1e060a5a.

--- arch/ia64/mm/hugetlbpage.c
+++ arch/ia64/mm/hugetlbpage.c
@@ -130,36 +130,6 @@ void hugetlb_free_pgd_range(struct mmu_gather *tlb,
 	free_pgd_range(tlb, addr, end, floor, ceiling);
 }
 
-unsigned long hugetlb_get_unmapped_area(struct file *file, unsigned long addr, unsigned long len,
-		unsigned long pgoff, unsigned long flags)
-{
-	struct vm_unmapped_area_info info;
-
-	if (len > RGN_MAP_LIMIT)
-		return -ENOMEM;
-	if (len & ~HPAGE_MASK)
-		return -EINVAL;
-
-	/* Handle MAP_FIXED */
-	if (flags & MAP_FIXED) {
-		if (prepare_hugepage_range(file, addr, len))
-			return -EINVAL;
-		return addr;
-	}
-
-	/* This code assumes that RGN_HPAGE != 0. */
-	if ((REGION_NUMBER(addr) != RGN_HPAGE) || (addr & (HPAGE_SIZE - 1)))
-		addr = HPAGE_REGION_BASE;
-
-	info.flags = 0;
-	info.length = len;
-	info.low_limit = addr;
-	info.high_limit = HPAGE_REGION_BASE + RGN_MAP_LIMIT;
-	info.align_mask = PAGE_MASK & (HPAGE_SIZE - 1);
-	info.align_offset = 0;
-	return vm_unmapped_area(&info);
-}
-
 static int __init hugetlb_setup_sz(char *str)
 {
 	u64 tr_pages;
