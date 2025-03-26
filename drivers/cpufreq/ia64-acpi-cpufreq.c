20cfa2d4af98ba91c01e58e0cfee8bf52e3a70b7
Johnny Mnemonic <jm@machine-hall.org>
2025-03-26 12:46:45 +0100

    ia64: handle "cpufreq: Remove cpufreq_generic_attrs"
    See 486729c6012042c486db2a5e4d5dd034fb1d3f3c.
    
    Also see for example 63c778aa1598c62971e7523c2a540da40cee1d70.

--- drivers/cpufreq/ia64-acpi-cpufreq.c
+++ drivers/cpufreq/ia64-acpi-cpufreq.c
@@ -326,7 +326,6 @@ static struct cpufreq_driver acpi_cpufreq_driver = {
 	.init		= acpi_cpufreq_cpu_init,
 	.exit		= acpi_cpufreq_cpu_exit,
 	.name		= "acpi-cpufreq",
-	.attr		= cpufreq_generic_attr,
 };
 
 
