74a8c5c2989c126d0fd8ad917b0c3c85f1ca707d
Johnny Mnemonic <jm@machine-hall.org>
2025-06-05 15:58:01 +0200

    ia64: replay "syscall.h: introduce syscall_set_nr()"
    See cc6622730be77fa88acc4fb0942cd39e6fa5ca27.

--- arch/ia64/include/asm/syscall.h
+++ arch/ia64/include/asm/syscall.h
@@ -23,6 +23,14 @@ static inline long syscall_get_nr(struct task_struct *task,
 	return regs->r15;
 }
 
+static inline void syscall_set_nr(struct task_struct *task,
+				  struct pt_regs *regs,
+				  int nr)
+{
+	regs->r15 = nr;
+}
+
+
 static inline void syscall_rollback(struct task_struct *task,
 				    struct pt_regs *regs)
 {
